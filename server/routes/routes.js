6f3de38cf04ec9133025c3d8183ffab08977793e
CHidave291 <dukelere@gmail.com>
2019-07-15 07:37:39 +0100

    feature(signin): user can signin  [Finishes #167188106]

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { allUsers, signup } from '../controllers/usersController';
+import { signin } from '../controllers/signinController';
 import { middleware } from '../middleware/middleware';
 
 const router = express.Router();
 
 router.route('/auth/signup').post(signup);
 router.route('/auth/signup').get(allUsers);
+router.route('/auth/signin').post(signin);
 
 export default router;
